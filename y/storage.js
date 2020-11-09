6b7ccede7fee1994814633ebba1fd79c59119f0a
cseibert2667 <cseibert2667@gmail.com>
2020-11-09 12:28:44 -0700

    read/write firebase storage

--- y/storage.js
+++ y/storage.js
@@ -4,4 +4,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
    
   });
 
-  
+function uploadFile(files) {
+    const storageRef = firebase.storage().ref();
+    const imgRef = storageRef.child("myimage.jpg");
+    const file = files.item(0);
+    const task = imgRef.put(file);
+    
+        task.then(snapshot => {
+            console.log(snapshot);
+            imgRef.getDownloadURL().then(url => {
+                document.querySelector("#imgUpload").setAttribute("src", url)
+            })
+        })
+    }
