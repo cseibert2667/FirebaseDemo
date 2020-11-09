deb71a01acb67867bfcd2aa4b75c2e6e2b0f41a2
cseibert2667 <cseibert2667@gmail.com>
2020-11-09 11:43:16 -0700

    file renames

--- y/storage.js
+++ y/storage.js
@@ -1,19 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    // check to ensure firebase is available
     const app = firebase.app();
-  
-    const db = firebase.firestore();
-  
-    const productsRef = db.collection("products");
-  
-    const query = productsRef.where("price", ">", 100);
-    // returns products in descending order of price -- .limit(1) will only return one item
-    // const query = productsRef.orderBy("price", "desc").limit(1);
-
-    query.get().then(products => {
-        products.forEach(doc => {
-            data = doc.data()
-            document.write(`${data.name} at $${data.price} <br />`)
-        })
-    })
-  
+   
   });
+
+  
